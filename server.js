366be6f04b68f5596cd9710115ab9bd235fbdca6
hthomp15 <hailethomp@gmail.com>
2022-06-02 11:05:06 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
 const path = require('path');
-require('dotenv').config();
 const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
-
-// Initialize handlebars for the html templates
-const hbs = exphbs.create({ helpers });
-
-// Initialize sessions
-const sess = {
-    secret: process.env.DB_SESSION_SECRET,
-    cookie: { maxAge: 7200000 },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+const session = require('express-session');
+//code to set up Handlebars.js as the app's template engine of choice
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+
+const minute = 60000
+const sess = {
+  secret: process.env.secret,
+  //timeout after 5 minutes
+  cookie: { maxAge: minute * 5 },
+  rolling: true,
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.use(session(sess))
+
+const helpers = require('./utils/helpers')
+
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(session(sess));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+sequelize.sync({ force: false})
+.then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
